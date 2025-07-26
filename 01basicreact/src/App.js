b757692bc36f373563a82e6971bf072f783f378f
Abdul-Mueed54 <mueed9972@gmail.com>
2025-07-26 11:42:19 +0000

    custom react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+
+
+function App() {
+  return (
+    <h1>CHAI OR REACT</h1>
+  )};
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+   <h1>hello from vite</h1>
+  )
+}
+
+export default App
